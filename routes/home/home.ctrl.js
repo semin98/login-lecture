c0cf685e031f908737220de7eba326d77a0322a1
semin98 <semin98@naver.com>
2023-07-18 21:00:42 +0900

    깃 저장소 초기화

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,14 @@
+"use stirct";
+
+const hello = (req,res)=> {
+    res.render("home/index");
+};
+
+const login = (req,res)=>{
+    res.render("home/login");
+}
+
+module.exports = {
+    hello,
+    login,
+};
